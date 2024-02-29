00e0dae96ab3f885f45eb0a565735217874d8c93
Rafael Carneiro Pregardier <rcpregardier@inf.ufsm.br>
2024-02-29 16:33:47 -0300

    Initial commit

---
+++ src/main/java/net/pre9o/testemod/TesteMod.java
@@ -0,0 +1,53 @@
+package net.pre9o.testemod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(TesteMod.MOD_ID)
+public class TesteMod{
+    public static final String MOD_ID = "testemod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public TesteMod(){
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event){
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents{
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){
+
+        }
+    }
+}
