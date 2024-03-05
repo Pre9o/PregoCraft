6295eb0fd02527867cd4be62dcb581d88c26f76d
Rafael Carneiro Pregardier <rcpregardier@inf.ufsm.br>
2024-03-05 15:23:43 -0300

    safira bloco

---
+++ src/main/java/net/pre9o/testemod/item/ModCreativeModTabs.java
@@ -0,0 +1,38 @@
+package net.pre9o.testemod.item;
+
+import net.pre9o.testemod.TesteMod;
+import net.pre9o.testemod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TesteMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+                        pOutput.accept(Items.DIAMOND);
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                    })
+                    .build());
+
+
+    public static void register(IEventBus eventBus) {
+
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
